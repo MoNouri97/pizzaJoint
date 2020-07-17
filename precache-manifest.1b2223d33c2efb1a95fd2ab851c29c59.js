8f26e6f5784588d184f33c17da3d5f8a26ed2a8a
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2020-07-17 18:25:36 +0100

    Updates

---
+++ precache-manifest.1b2223d33c2efb1a95fd2ab851c29c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddba24e7bac3bed15f4bbe37fffb808",
+    "url": "/pizzaJoint/index.html"
+  },
+  {
+    "revision": "815147ded84301bc8165",
+    "url": "/pizzaJoint/static/css/main.af78a941.chunk.css"
+  },
+  {
+    "revision": "58b471ac79fe514d69c6",
+    "url": "/pizzaJoint/static/js/2.d897a650.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pizzaJoint/static/js/2.d897a650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815147ded84301bc8165",
+    "url": "/pizzaJoint/static/js/main.1bee5dd6.chunk.js"
+  },
+  {
+    "revision": "28b753ddca95f9e8cc4f",
+    "url": "/pizzaJoint/static/js/runtime-main.28ba6d7c.js"
+  }
+]);
